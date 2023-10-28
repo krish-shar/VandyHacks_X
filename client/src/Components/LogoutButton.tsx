476bbf5981a8e38f5c2bcacff9ec0b79713c371a
Sahit Mamidipaka <smamidipaka6@gatech.edu>
2023-10-28 18:20:48 -0500

    created logout button w/ Auth0

---
+++ client/src/Components/LogoutButton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mui/material";
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <Button
+      variant="contained"
+      onClick={() =>
+        logout({
+          logoutParams: {
+            returnTo: "https://pornhub.com/",
+            client_id: "XKstvdcjCKBZgYFVGsgXwjkCcRbQvODK",
+          },
+        })
+      }
+    >
+      Log Out
+    </Button>
+  );
+};
+
+export default LogoutButton;
