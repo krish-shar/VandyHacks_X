42a6f77c159ca787a3440c96ea8224ada42d17e9
Sahit Mamidipaka <smamidipaka6@gatech.edu>
2023-10-28 18:21:23 -0500

    added Auth0 react wrapper for App

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@emotion/react';
 import '@mui/material';
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-rlbnqg405rl5zmak.us.auth0.com"
+      clientId="XKstvdcjCKBZgYFVGsgXwjkCcRbQvODK"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
