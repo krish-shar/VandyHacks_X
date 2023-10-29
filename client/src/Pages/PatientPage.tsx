ef00a403785764f0708c145f96d0e7ffcb9e3f18
Hari Jayasankar <harij2266@gmail.com>
2023-10-28 20:41:38 -0400

    Added Cards with medicine details

---
+++ client/src/Pages/PatientPage.tsx
@@ -0,0 +1,11 @@
+import {MedCards} from '../Components/MedCards';
+
+function PatientPage() {
+    return (
+        <div>
+            <MedCards />
+        </div>
+    );
+}
+
+export default PatientPage;
