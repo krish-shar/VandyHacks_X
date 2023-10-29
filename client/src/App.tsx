f8c7270df8fffbdcbebee803fb57068746367f0c
krish-shar <krishsharma2308@gmail.com>
2023-10-28 20:28:48 -0400

    UI Improvements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,49 +1,31 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-//
-// function App() {
-//   const [count, setCount] = useState(0)
-//
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-//
-// export default App
+import RelativeButton from "./Components/RelativeButton";
 
-import Cam from "./Components/Camera.tsx";
+function App() {
 
-export default function App() {
-    return (
 
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <Cam />
+  return (
+    <div className="App">
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="flex flex-col justify-center text-center">
+          <h1 className="text-2xl text-white">Welcome to the Healthcare System</h1>
+          <h1 className="text-2xl text-white">Select your role:</h1>
+          <br />
         </div>
-    )
+
+
+        <div className="flex flex-row justify-center">
+          <div className="flex flex-col gap-y-4">
+          <RelativeButton label="Patient" onClick={() => {
+        
+          }}  large />
+            <RelativeButton label="Doctor" onClick={() => {}} large disabled secondary />
+
+            <RelativeButton label="Relative" onClick={() => {}}  large disabled secondary/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default App;
