e51aee39bc37f3eec4990c560a9859d2edd402e0
krish-shar <krishsharma2308@gmail.com>
2023-10-29 01:05:29 -0400

    Updated ui, added cards as well as status indicator

--- client/src/Pages/Dashboard.tsx
+++ client/src/Pages/Dashboard.tsx
@@ -1,8 +1,15 @@
 import Header from '../Components/Header';
 import { useAuth0 } from "@auth0/auth0-react";
+import MedCards from '../Components/MedCards';
+import { medicine } from '../Components/MockMedicine';
 
 function Dashboard() {
   const { user } = useAuth0();
+
+
+  const medCards = medicine.map((med) => <MedCards name={med.name} dosage={med.dosage} frequency={med.frequency} interval={med.interval} taken={med.taken} status={med.status}/>)
+
+
 
   return (
     <main className="bg-gray-900 text-white min-h-screen">
@@ -10,7 +17,18 @@ function Dashboard() {
       <section className="container mx-auto p-4">
         <h2 className="text-2xl font-bold">Your medicines!</h2>
         <p>Welcome, {user?.name}!</p>
+        
+        <div className='py-2'>
+          <div className="bg-slate-800 rounded-xl p-4">
+            <h2 className="text-white text-xl font-semibold">Today!</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              {medCards}
+            </div>
+          </div>
+        </div>
+    
       </section>
+      
     </main>
   );
 }
