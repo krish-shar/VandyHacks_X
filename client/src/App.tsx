7094a1e9abd8dc615bd86e113e4ae0d0a5bab068
Sahit Mamidipaka <smamidipaka6@gatech.edu>
2023-10-28 21:57:42 -0500

    minor changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,30 @@
-import Doctor from './Components/Doctor';
-import Relative from './Components/Relative';
-import Patient from './Components/Patient';
-import Grid from '@mui/material/Grid'
-import LoginButton from './Components/LoginButton';
-import LogoutButton from './Components/LogoutButton';
+import Doctor from "./Components/Doctor";
+import Relative from "./Components/Relative";
+import Patient from "./Components/Patient";
+import Grid from "@mui/material/Grid";
+import LoginButton from "./Components/LoginButton";
+import LogoutButton from "./Components/LogoutButton";
+
 
 function App() {
-    return (
-      <Grid container 
-      justifyContent="center" 
-      alignItems="center" 
+
+  return (
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
       direction="column"
-      style={{height: "100vh"}}
-      >
-        <h1>Who are you?</h1>
-        <Doctor />
-        <Relative />
-        <Patient />
-        <LoginButton></LoginButton>
-        <br></br>
-        <LogoutButton></LogoutButton>
-      </Grid>
-    );
+      style={{ height: "100vh" }}
+    >
+      <h1>Who are you?</h1>
+      <Doctor />
+      <Relative />
+      <Patient />
+      <LoginButton></LoginButton>
+      <br></br>
+      <LogoutButton></LogoutButton>
+    </Grid>
+  );
 }
 
 export default App;
